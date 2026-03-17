0e31a726aeca7c8a38983b29f0221bdafc7f0770
lankut <lankute@mail.ru>
2026-03-17 11:45:53 +0300

    PJM-29 feat: implement reusable Checkbox component with all states and variants

--- src/shared/ui/Checkbox/Checkbox.stories.tsx
+++ src/shared/ui/Checkbox/Checkbox.stories.tsx
@@ -1,63 +1,105 @@
-import type { Meta, StoryObj } from '@storybook/nextjs-vite'
-import { Checkbox } from './Checkbox'
+import type {Meta, StoryObj} from '@storybook/nextjs-vite'
+import {CheckBox} from '@/shared/ui/Checkbox'
+import React from "react";
 
-const meta: Meta<typeof Checkbox> = {
-  title: 'UI/Checkbox',
-  component: Checkbox,
-  tags: ['autodocs'],
-  argTypes: {
-    size: {
-      control: 'select',
-      options: ['sm', 'md', 'lg'],
+const meta: Meta<typeof CheckBox> = {
+    title: 'Shared/UI/Checkbox',
+    component: CheckBox,
+
+    parameters: {
+        layout: 'centered',
+        backgrounds: {
+            default: 'inctagram-dark',
+            values: [
+                {
+                    name: 'inctagram-dark',
+                    value: '#0d0d0d',
+                },
+            ],
+        },
     },
-    disabled: {
-      control: 'boolean',
+    decorators: [
+        Story => (
+            <div style={{backgroundColor: '#0d0d0d', padding: '24px'}}>
+                <Story/>
+            </div>
+        ),
+    ],
+    args: {
+        label: 'Check-box',
+        size: 'md',
     },
-    error: {
-      control: 'boolean',
+    argTypes: {
+        className: {
+            control: false,
+        },
+        size: {
+            control: {type: 'inline-radio'},
+            options: ['sm', 'md', 'lg'],
+        },
     },
-  },
-}
+} satisfies Meta<typeof CheckBox>
 
 export default meta
 
-type Story = StoryObj<typeof Checkbox>
+type Story = StoryObj<typeof CheckBox>
 
 export const Default: Story = {
-  args: {
-    label: 'Checkbox',
-    size: 'md',
-    disabled: false,
-  },
+    args: {
+        label: 'Checkbox',
+        size: 'md',
+        disabled: false,
+    },
 }
 
 export const Checked: Story = {
-  args: {
-    label: 'Checked checkbox',
-    defaultChecked: true,
-  },
+    args: {
+        label: 'Checked checkbox',
+        checked: true,
+    },
 }
 
 export const Disabled: Story = {
-  args: {
-    label: 'Disabled checkbox',
-    disabled: true,
-  },
+    args: {
+        label: 'Disabled checkbox',
+        disabled: true,
+    },
+}
+
+export const DisabledChecked: Story = {
+    args: {
+        label: 'Disabled checkbox',
+        disabled: true,
+        checked: true
+    },
 }
 
 export const Error: Story = {
-  args: {
-    label: 'Checkbox with error',
-    error: true,
-  },
+    args: {
+        label: 'Checkbox with error',
+        error: true,
+    },
+}
+
+export const ControlledCheckbox: StoryObj<typeof CheckBox> = {
+    render: () => {
+        const [checked, setChecked] = React.useState<boolean | 'indeterminate'>(false)
+        return (
+            <CheckBox
+                label="ControlledCheckbox"
+                checked={checked}
+                onCheckedChange={setChecked}
+            />
+        )
+    },
 }
 
 export const Sizes: Story = {
-  render: () => (
-    <div style={{ display: 'flex', gap: 20 }}>
-      <Checkbox size="sm" label="Small" />
-      <Checkbox size="md" label="Medium" />
-      <Checkbox size="lg" label="Large" />
-    </div>
-  ),
+    render: () => (
+        <div style={{display: 'flex', gap: 20}}>
+            <CheckBox size="sm" label="Small"/>
+            <CheckBox size="md" label="Medium"/>
+            <CheckBox size="lg" label="Large"/>
+        </div>
+    ),
 }
