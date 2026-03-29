62c693b99d1eba438211d7ecec206c2e087bec0c
dyerman456 <dyerman456@gmail.com>
2026-03-29 17:41:28 +0200

    PJM-8 fix(Card): add props into card

--- src/shared/ui/Card/Card.tsx
+++ src/shared/ui/Card/Card.tsx
@@ -1,8 +1,13 @@
 import { ComponentPropsWithoutRef, ReactNode } from 'react'
 import s from './Card.module.scss'
+import clsx from 'clsx'
 
 type Props = {
   children: ReactNode
 } & ComponentPropsWithoutRef<'section'>
 
-export const Card = ({ children }: Props) => <section className={s.container}>{children}</section>
+export const Card = ({ children, className, ...rest }: Props) => (
+  <section className={clsx(s.container, className)} {...rest}>
+    {children}
+  </section>
+)
