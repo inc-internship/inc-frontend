f317dfdb87273a2c3cca455a594df0bc24720878
ThunderInTheGrass <propeller8924@gmail.com>
2026-03-31 09:34:29 +0800

    PJM-64 refactor: update forgot passwford form

--- src/views/forgot-password/ui/ForgotPasswordPage.tsx
+++ src/views/forgot-password/ui/ForgotPasswordPage.tsx
@@ -1,5 +1,20 @@
+import { Card } from '@/shared/ui/Card'
+import { Typography } from '@/shared/ui/Typography'
 import { ForgotPasswordForm } from '@/features/forgot-password/ui/ForgotPasswordForm'
+import s from './ForgotPasswordPage.module.scss'
+import Link from 'next/link'
+import { Button } from '@/shared/ui/Button'
 
-export const ForgotPasswordPage = () => {
-  return <ForgotPasswordForm />
-}
+export const ForgotPasswordPage = () => (
+  <Card>
+    <Typography variant="h1" align="center" className={s.title}>
+      Forgot Password
+    </Typography>
+    <ForgotPasswordForm />
+    <Button asChild={true} variant="default" className={s.signInButton}>
+      <Link href="/sign-in" className={s.link}>
+        Back to Sign In
+      </Link>
+    </Button>
+  </Card>
+)
