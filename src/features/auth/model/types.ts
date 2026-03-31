6e6a636c7bbbe1533fd702d8cd0e03c9df9519e7
Alena Alioshyna <elenakovalski1995@yandex.ru>
2026-03-31 20:45:01 +0300

    PJM-69 chore(auth): shorten import path in auth types

--- src/features/auth/model/types.ts
+++ src/features/auth/model/types.ts
@@ -1,6 +1,6 @@
 import z from 'zod/v4'
 import { loginFormSchema } from '@/features/auth'
-import { createNewPasswordFormSchema } from './create-new-password-form-creator'
+import { createNewPasswordFormSchema } from '@/features/auth'
 
 export type LoginFormField = z.infer<typeof loginFormSchema>
 export type CreateNewPasswordFormField = z.infer<typeof createNewPasswordFormSchema>
