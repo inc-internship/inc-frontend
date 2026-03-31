bcc44acc51eccbf2118cee01a8806c933234987d
Alena Alioshyna <elenakovalski1995@yandex.ru>
2026-03-31 20:38:59 +0300

    PJM-69 feat(auth): add create new password zod schema and form type

--- src/features/auth/model/types.ts
+++ src/features/auth/model/types.ts
@@ -1,4 +1,6 @@
 import z from 'zod/v4'
 import { loginFormSchema } from '@/features/auth'
+import { createNewPasswordFormSchema } from './create-new-password-form-creator'
 
 export type LoginFormField = z.infer<typeof loginFormSchema>
+export type CreateNewPasswordFormField = z.infer<typeof createNewPasswordFormSchema>
