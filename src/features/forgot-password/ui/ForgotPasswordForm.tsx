25257d6ecee7d361dc3e375b630e4894ab97a4f3
ThunderInTheGrass <propeller8924@gmail.com>
2026-03-31 10:17:01 +0800

    PJM-64 refactor: add resend instruction text, styles

--- src/features/forgot-password/ui/ForgotPasswordForm.tsx
+++ src/features/forgot-password/ui/ForgotPasswordForm.tsx
@@ -12,6 +12,9 @@ export const ForgotPasswordForm = () => (
       <Typography variant="text-m" className={s.assistText}>
         Enter your email address and we will send you further instructions
       </Typography>
+      <Typography variant="text-m" className={s.resendInstructionText}>
+        The link has been sent by email. If you don’t receive an email send link again
+      </Typography>
       <Button variant="primary" type="submit" fullWidth={true} className={s.submitButton}>
         Send Link
       </Button>
