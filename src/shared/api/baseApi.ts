683f7bca34141ada37517eef9c447e3c8702810b
dyerman456 <dyerman456@gmail.com>
2026-04-05 14:15:15 +0200

    PJM-8(fix) feat: attach a token to each request in app

--- src/shared/api/baseApi.ts
+++ src/shared/api/baseApi.ts
@@ -1,10 +1,25 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 import { BASE_URL } from '@/shared/constants'
 
+type BaseApiState = {
+  auth?: {
+    accessToken?: string | null
+  }
+}
+
 export const baseApi = createApi({
   reducerPath: 'baseApi',
   baseQuery: fetchBaseQuery({
     baseUrl: BASE_URL,
+    prepareHeaders: (headers, api) => {
+      const token = (api.getState() as BaseApiState).auth?.accessToken
+
+      if (token) {
+        headers.set('Authorization', `Bearer ${token}`)
+      }
+
+      return headers
+    },
   }),
   endpoints: () => ({}),
 })
