162529f01d07ccb9ba5529ff67d4b347eec64129
ThunderInTheGrass <propeller8924@gmail.com>
2026-04-08 19:09:33 +0800

    PJM-78 feat: add reusable component Tabs with styles, responsive and stories

--- src/views/profile-settings/ui/ProfileSettingsPage.tsx
+++ src/views/profile-settings/ui/ProfileSettingsPage.tsx
@@ -1,5 +1,46 @@
 'use client'
 
+import s from './ProfileSettingsPage.module.scss'
+import { ProfileInformation } from '@/views/profile-settings/ui/profile-information/ProfileInformation'
+import { Tabs } from '@/shared/ui/Tabs'
+import { useState } from 'react'
+import { ProfileManagement } from '@/views/profile-settings/ui/profile-management/ProfileManagement'
+import { ProfileDevices } from '@/views/profile-settings/ui/profile-devices/ProfileDevices'
+import { ProfilePayments } from '@/views/profile-settings/ui/profile-payments/ProfilePayments'
+
 export const ProfileSettingsPage = () => {
-  return <></>
+  const [activeTab, setActiveTab] = useState('1')
+
+  const tabs = [
+    {
+      content: <ProfileInformation />,
+      title: 'GeneralInformation',
+      value: '1',
+    },
+    {
+      content: <ProfileDevices />,
+      title: 'Devices',
+      value: '2',
+    },
+    {
+      content: <ProfileManagement />,
+      title: 'AccountManagement',
+      value: '3',
+    },
+    {
+      content: <ProfilePayments />,
+      title: 'MyPayments',
+      value: '4',
+    },
+  ]
+
+  const onValueChangeHandler = (value: string) => {
+    setActiveTab(value)
+  }
+
+  return (
+    <div className={s.container}>
+      <Tabs className={s.tabs} tabs={tabs} value={activeTab} onValueChange={onValueChangeHandler} />
+    </div>
+  )
 }
---
+++ src/views/profile-settings/ui/profile-devices/ProfileDevices.tsx
@@ -0,0 +1,3 @@
+export const ProfileDevices = () => {
+  return <div>Девайсы</div>
+}
