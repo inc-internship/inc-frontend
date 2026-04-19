4bbbe477b2db3f29feabe6a5f0a3b033090b3fcb
Alena Alioshyna <elenakovalski1995@yandex.ru>
2026-04-19 13:33:36 +0300

    PJM-103 feat(i18n-core): add provider and language resources

--- src/app/providers/index.tsx
+++ src/app/providers/index.tsx
@@ -1,10 +1,16 @@
 import type { ReactNode } from 'react'
 import { StoreProvider } from './store'
+import { I18nProvider, type Locale } from '@/shared/i18n'
 
 type AppProvidersProps = Readonly<{
   children: ReactNode
+  initialLocale: Locale
 }>
 
-export function AppProviders({ children }: AppProvidersProps) {
-  return <StoreProvider>{children}</StoreProvider>
+export function AppProviders({ children, initialLocale }: AppProvidersProps) {
+  return (
+    <I18nProvider initialLocale={initialLocale}>
+      <StoreProvider>{children}</StoreProvider>
+    </I18nProvider>
+  )
 }
