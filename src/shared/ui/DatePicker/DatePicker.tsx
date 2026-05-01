4dd27cdcb87d442ef6828565166dfec0b2eb459f
Dzmitry Poltaradnev <poltaradnev@gmail.com>
2026-05-01 12:34:21 +0400

    PJM-139 feat: add custom components to dayPicker

--- src/shared/ui/DatePicker/DatePicker.tsx
+++ src/shared/ui/DatePicker/DatePicker.tsx
@@ -3,6 +3,9 @@
 import { format } from 'date-fns'
 import s from './DatePicker.module.scss'
 import { type DateRange, DayPicker } from 'react-day-picker'
+import { Calendar } from '@/shared/ui/DatePicker/DatePickerIcon/Calendar'
+import { ArrowLeft } from '@/shared/ui/DatePicker/DatePickerIcon/ArrowLeft'
+import { ArrowRight } from '@/shared/ui/DatePicker/DatePickerIcon/ArrowRight'
 
 export type DatePickerProps =
   | {
@@ -22,6 +25,19 @@ export type DatePickerProps =
       disabled?: boolean
     }
 
+const dayPickerComponents = {
+  PreviousMonthButton: (props: React.ComponentProps<'button'>) => (
+    <button {...props} className={s.navButton} aria-label="Previous month">
+      <ArrowLeft />
+    </button>
+  ),
+  NextMonthButton: (props: React.ComponentProps<'button'>) => (
+    <button {...props} className={s.navButton} aria-label="Next month">
+      <ArrowRight />
+    </button>
+  ),
+}
+
 export const DatePicker = (props: DatePickerProps) => {
   const inputValue =
     props.mode === 'single'
@@ -38,12 +54,13 @@ export const DatePicker = (props: DatePickerProps) => {
 
       <button type="button" className={s.trigger} disabled={props.disabled}>
         {inputValue || 'Date select'}
-        <span className={s.icon}>📅</span>
+        <Calendar />
       </button>
 
       <div className={s.popover}>
         {props.mode === 'single' ? (
           <DayPicker
+            components={dayPickerComponents}
             mode="single"
             selected={props.value}
             onSelect={props.onChange}
@@ -62,6 +79,7 @@ export const DatePicker = (props: DatePickerProps) => {
           />
         ) : (
           <DayPicker
+            components={dayPickerComponents}
             mode="range"
             selected={props.value}
             onSelect={props.onChange}
