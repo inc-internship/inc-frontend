901306e3036f5fd39d10b8a005fc84ecfb7eca76
dyerman456 <dyerman456@gmail.com>
2026-05-27 18:56:33 +0200

    PJM-135 fix: fix build error

--- src/features/view-post/ui/ViewPostModal/ViewPostModal.tsx
+++ src/features/view-post/ui/ViewPostModal/ViewPostModal.tsx
@@ -34,7 +34,6 @@ export const ViewPostModal = ({ open, post, menuItems = [], onCancel }: Props) =
       onOpenChange={nextOpen => !nextOpen && onCancel()}
       className={s.container}
       closeOnOverlay={false}
-      onOpenAutoFocus={e => e.preventDefault()}
     >
       <ModalClose className={s.close}>
         <CloseIcon />
